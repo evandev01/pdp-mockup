8719d615a712c7534bcf972e1a5199fb174fb7b3
ebero <ebero@IBGDEV>
2022-12-09 11:59:42 -0800

    commit 12/09/2022

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-window.scroll({
-  behavior: "smooth",
-})
+// $("#ingredients").scrollIntoView({ behavior: "smooth" })
+// $("#productInfo").scrollIntoView({ behavior: "smooth" })
+
+// $(".tablinks").click(function () {
+//   console.log("clicked")
+// })
+// window.scroll({
+//   behavior: "smooth",
+// })
