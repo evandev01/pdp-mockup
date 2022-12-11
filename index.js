fee553ec4667edcad29a16bed9b48f3e53854c64
Evan Bero <63209633+evandev01@users.noreply.github.com>
2022-12-10 18:48:35 -0600

    styled tab links with jquery hover effect

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-// $("#ingredients").scrollIntoView({ behavior: "smooth" })
-// $("#productInfo").scrollIntoView({ behavior: "smooth" })
-
-// $(".tablinks").click(function () {
-//   console.log("clicked")
-// })
 // window.scroll({
 //   behavior: "smooth",
 // })
+
+$('.tab-btn-div').hover(
+	function () {
+		$(this).find('span').css('color', 'black')
+		$(this).find('#caret-down').css('color', 'black')
+	},
+	function () {
+		$(this).find('span').css('color', 'rgba(0, 0, 0, 0.377)')
+		$(this).find('#caret-down').css('color', '#27ae60')
+	}
+)
