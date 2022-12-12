eadc423fd855d2b5826af0623ef6104c4dbee28c
ebero <ebero@IBGDEV>
2022-12-12 08:33:48 -0800

    styled mobile responsive

--- index.js
+++ index.js
@@ -2,30 +2,30 @@
 //   behavior: "smooth",
 // })
 
-$('.tab-btn-div').hover(
-	function () {
-		$(this).find('span').css('color', 'black')
-		$(this).find('#caret-down').css('color', 'black')
-	},
-	function () {
-		$(this).find('span').css('color', 'rgba(0, 0, 0, 0.377)')
-		$(this).find('#caret-down').css('color', '#27ae60')
-	}
+$(".tab-btn-div").hover(
+  function () {
+    $(this).find("span").css("color", "rgba(0, 0, 0, 0.87)")
+    $(this).find("#caret-down").css("color", "rgba(0, 0, 0, 0.87)")
+  },
+  function () {
+    $(this).find("span").css("color", "rgba(0, 0, 0, 0.377)")
+    $(this).find("#caret-down").css("color", "#27ae60")
+  }
 )
 
-window.addEventListener('scroll', function () {
-	checkY()
+window.addEventListener("scroll", function () {
+  checkY()
 })
 
 function checkY() {
-	if (
-		window.scrollY > document.getElementById('pdp-top-container').offsetHeight
-	) {
-		console.log('past container')
-		$('.sticky-div-fixed').addClass('active')
-		// $('.sticky-div-fixed > img').addClass('active')
-	} else {
-		$('.sticky-div-fixed').removeClass('active')
-		// $('.sticky-div-fixed > img').removeClass('active')
-	}
+  if (
+    window.scrollY > document.getElementById("pdp-top-container").offsetHeight
+  ) {
+    console.log("past container")
+    $(".sticky-div-fixed").addClass("active")
+    // $('.sticky-div-fixed > img').addClass('active')
+  } else {
+    $(".sticky-div-fixed").removeClass("active")
+    // $('.sticky-div-fixed > img').removeClass('active')
+  }
 }
